c60ef8e6cd7ecaa08bacba8d1b6447a78d4d3988
Guillermo Vicente <84589575+guillermovicenteprieto@users.noreply.github.com>
2022-07-10 03:35:54 -0300

    routes carritos / carrito

--- src/routes/routeCart.js
+++ src/routes/routeCart.js
@@ -7,18 +7,21 @@ import { Router } from "express";
 // import CartController from "../controllers/cartController.js";
 // const cartController = new CartController();
 export const routeCart = Router();
-// routeCart.get("/carrito", cartController.getAllCarts);
+// routeCart.get("/carritos", cartController.getAllCarts);
 // routeCart.get("/carrito/:id", cartController.getCartById);
+// routeCart.get("/carrito/:id/productos", cartController.getCartByIdProducts);
 // routeCart.post("/carrito", cartController.createCart);
-// routeCart.post("/carrito/:id/productos", cartController.addProductToCart);
-// routeCart.put("/carrito/:id", cartController.updateCart);
+// routeCart.post("carrito/:id/productos/:idProduct", cartController.addProductToCart);
 // routeCart.delete("/carrito/:id", cartController.deleteCart);
+// routeCart.delete("/carrito/:id/productos/:idProduct  ", cartController.deleteProductFromCart);
+
+
 
 //export const routeCart = Router();
 /*============================[Rutas API: /api/carrito]============================*/
 
 routeCart
-    .get('/carrito', (req, res) => {
+    .get('/carritos', (req, res) => {
         logger.info(`Se registra petición GET /carritos`)
         Cart.find({}, (err, carritos) => {
             if (err) {
@@ -44,6 +47,34 @@ routeCart
         })
     })
 
+
+    .get('/carrito/:id/productos', (req, res) => {
+        logger.info(`Se registra petición GET /carritos/${req.params.id}/productos`)
+        Cart.findById(req.params.id, (err, carrito) => {
+            if (err) {
+                logger.error(`Error al obtener productos`)
+                res.json(err);
+            } else {
+                Product.find({ _id: { $in: carrito.products } }, (err, productos) => {
+                    if (err) {
+                        logger.error(`Error al obtener productos`)
+                        res.json(err);
+                    } else {
+                        logger.info(`Se obtienen productos`)
+                        const cart = {
+                            id: carrito.id,
+                            name: carrito.name,
+                            Date: carrito.Date,
+                            user: carrito.user,
+                            products: productos
+                        }
+                        res.json({ cart });
+                    }
+                })
+            }
+        })
+    })
+
     .post('/carrito', (req, res) => {
         logger.info(`Se registra petición POST /carritos`)
         const carrito = {
@@ -64,22 +95,38 @@ routeCart
         })
     })
 
-    .put('/carrito/:id', (req, res) => {
-        logger.info(`Se registra petición PUT /carritos/${req.params.id}`)
-        const carrito = {
-            id: req.body.id,
-            name: req.body.name,
-            Date: req.body.Date,
-            user: req.body.user,
-            products: req.body.products
-        }
-        Cart.findByIdAndUpdate(req.params.id, carrito, (err, carrito) => {
+    .post('/carrito/:id/productos/:idProduct', (req, res) => {
+        logger.info(`Se registra petición POST /carritos/${req.params.id}/productos`)
+        Cart.findById(req.params.id, (err, carrito) => {
             if (err) {
-                logger.error(`Error al actualizar carrito`)
+                logger.error(`Error al obtener carrito`)
                 res.json(err);
             } else {
-                logger.info(`Se actualiza carrito`)
-                res.json(carrito);
+                Product.findById(req.params.idProduct, (err, product) => {
+                    if (err) {
+                        logger.error(`Error al obtener producto`)
+                        res.json(err);
+                    } else {
+                        carrito.products.push(product);
+                        carrito.save((err, carrito) => {
+                            if (err) {
+                                logger.error(`Error al agregar producto al carrito`)
+                                res.json(err);
+                            } else {
+                                logger.info(`Se agrega producto al carrito`)
+                                const cart = {
+                                    id: carrito.id,
+                                    name: carrito.name,
+                                    Date: carrito.Date,
+                                    user: carrito.user,
+                                    products: carrito.products
+                                }
+                                res.json({ cart, product });
+                                //res.json(carrito);
+                            }
+                        })
+                    }
+                })
             }
         })
     })
@@ -97,59 +144,40 @@ routeCart
         })
     })
 
-    .get('/carrito/:id/productos', (req, res) => {
-        logger.info(`Se registra petición GET /carritos/${req.params.id}/productos`)
+    .delete('/carrito/:id/productos/:idProduct', (req, res) => {
+        logger.info(`Se registra petición DELETE /carritos/${req.params.id}/products/${req.params.idProduct}`)
         Cart.findById(req.params.id, (err, carrito) => {
             if (err) {
-                logger.error(`Error al obtener productos`)
+                logger.error(`Error al obtener carrito`)
                 res.json(err);
             } else {
-                Product.find({ _id: { $in: carrito.products } }, (err, productos) => {
+                Product.findByIdAndDelete(req.params.idProduct, (err, product) => {
                     if (err) {
-                        logger.error(`Error al obtener productos`)
+                        logger.error(`Error al eliminar producto`)
                         res.json(err);
                     } else {
-
-                        logger.info(`Se obtienen productos`)
-                        res.json(carrito.products);
+                        carrito.products.pull(product);
+                        carrito.save((err, carrito) => {
+                            if (err) {
+                                logger.error(`Error al eliminar producto del carrito`)
+                                res.json(err);
+                            } else {
+                                logger.info(`Se elimina producto del carrito`)
+                                const cart = {
+                                    id: carrito.id,
+                                    name: carrito.name,
+                                    Date: carrito.Date,
+                                    user: carrito.user,
+                                    products: carrito.products
+                                }
+                                res.json({ cart });
+                                //res.json(carrito);
+                            }
+                        })
                     }
                 })
             }
         })
     })
-    .post('/carrito/:id/productos', (req, res) => {
-        logger.info(`Se registra petición POST /carritos/${req.params.id}/products`)
-        const product = {
-            id: req.body.id,
-            name: req.body.name,
-            price: req.body.price,
-            description: req.body.description
-        }
-
-        Cart.findByIdAndUpdate(req.params.id, { $push: { products: product } }, (err, carrito) => {
-            if (err) {
-                logger.error(`Error al agregar producto al carrito`)
-                res.json(err);
-            } else {
-                logger.info(`Se agrega producto al carrito`)
-                res.json(carrito);
-            }
-        })
-    })
-
-
-    .delete('/:id/productos/:idProduct', (req, res) => {
-        logger.info(`Se registra petición DELETE /carritos/${req.params.id}/products/${req.params.idProduct}`)
-        Cart.findByIdAndUpdate(req.params.id, { $pull: { products: { id: req.params.idProduct } } }, (err, carrito) => {
-            if (err) {
-                logger.error(`Error al eliminar producto del carrito`)
-                res.json(err);
-            } else {
-                logger.info(`Se elimina producto del carrito`)
-                res.json(carrito);
-            }
-        })
-    })
-
 
 export default routeCart;
